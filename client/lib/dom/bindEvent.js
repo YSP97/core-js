c50879b26bf6a82dc19953904bbaf6356e2ab9ce
YSP97 <pus9717@gmail.com>
2024-06-24 16:03:15 +0900

    주접 생성기 필드 추가

--- client/lib/dom/bindEvent.js
+++ client/lib/dom/bindEvent.js
@@ -1,6 +1,17 @@
-function bindEvent(node, type, handler) {
-  if (isString(node)) node = getNode(node);
-  node.addEventListener(type, handler);
+import { getNode } from "./getNode.js";
+import { isString } from "../utils/type.js";
 
-  return () => node.removeEventListener(type, handler);
+
+
+export function bindEvent(node,type,handler){
+  
+  if(isString(node)) node = getNode(node);
+
+  node.addEventListener(type,handler);
+
+  return () => node.removeEventListener(type,handler);
+
 }
+
+
+
--- client/main.js
+++ client/main.js
@@ -1,8 +1,28 @@
-// const a = 'hello';
+import data from './data/data.js';
+import clearContents from './lib/dom/clearContents.js';
+import { getNode, getRandom, insertLast } from './lib/index.js';
 
-// console.log('배고파');
+console.log(data);
 
-/* {#ff0}alert('마우스 우클릭 사용 금지'); */
+// [phase-1]
+// 1. 주접 떨기 버튼을 클릭하는 함수
+//    - 주접 떨기 버튼 가져오기
+//    - 이벤트 연결하기
 
-// // confirm('ㄹㅇ로...?');
-prompt('니이름이머니?');
+const submit = getNode('#submit');
+const nameField = getNode('#nameField');
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const name = nameField.value;
+  const list = data(name);
+  const pick = list[getRandom(list.length)];
+  const result = getNode('.result');
+  clearContents(result);
+  insertLast(result, pick);
+
+  nameField.value = ''
+}
+
+submit.addEventListener('click', handleSubmit);
