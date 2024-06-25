decc1944e359d5f44c2b3b426cb7a9c354ef4370
YSP97 <pus9717@gmail.com>
2024-06-25 11:47:56 +0900

    main.js 수정

--- client/main.js
+++ client/main.js
@@ -1,20 +1,48 @@
-// import {  } from './lib/index.js'; // 실행 안해도 일반 애니메이션으로 건 gsap그거는 이렇게 연결만 해도 실행이 된다고 함...
-import clearContents from './lib/dom/clearContents.js';
-import { endScroll } from './lib/dom/endScroll.js';
 import {
-  diceAnimation,
-  getNodes,
-  getNode,
   attr,
+  getNode,
+  getNodes,
+  endScroll,
   insertLast,
+  diceAnimation,
+  clearContents,
 } from './lib/index.js';
 
 // 1. 주사위 애니메이션
-// 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation 실행될 수 있도록
+// 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation() 실행될 수 있도록
 
-const btnGroupt = getNodes('.buttonGroup > button');
+const [rollingButton, recordButton, resetButton] = getNodes(
+  '.buttonGroup > button'
+);
+const recordListWrapper = getNode('.recordListWrapper');
 
-const [rollingDiceBtn, recordBtn, resetBtn] = btnGroupt;
+let count = 0;
+let total = 0;
+
+function createItem(value) {
+  const template = `
+    <tr>
+      <td>${++count}</td>
+      <td>${value}</td>
+      <td>${(total += value)}</td>
+    </tr>
+  `;
+  return template;
+}
+
+function renderRecordItem() {
+  // const diceValue = getNode('#cube').getAttribute('dice');
+  const diceValue = Number(attr(getNode('#cube'), 'dice'));
+
+  insertLast('.recordList tbody', createItem(diceValue));
+
+  // 1. insertLast 함수 사용
+
+  // 2. template 전달
+
+  // 3. diceValue interpolation(보간법) 하기 ex) <td>${diceValue}</td>
+  endScroll(recordListWrapper);
+}
 
 const handleRollingDice = (() => {
   let isClicked = false;
@@ -23,56 +51,30 @@ const handleRollingDice = (() => {
   return () => {
     if (!isClicked) {
       stopAnimation = setInterval(diceAnimation, 100);
-      recordBtn.disabled = true;
-      resetBtn.disabled = true;
+      recordButton.disabled = true;
+      resetButton.disabled = true;
     } else {
       clearInterval(stopAnimation);
-      recordBtn.disabled = false;
-      resetBtn.disabled = false;
+      recordButton.disabled = false;
+      resetButton.disabled = false;
     }
-
     isClicked = !isClicked;
   };
 })();
 
-const recordListWrapper = getNode('.recordListWrapper');
-let count = 0;
-let total = 0;
-
-function createItem(value) {
-  const template = /* html */ `
-  <tr>
-    <td>${++count}</td>
-    <td>${value}</td>
-    <td>${(total += value)}</td>
-  </tr>`;
-
-  return template;
-}
-
-function rederRecordItem() {
-  const diceValue = +attr(getNode('#cube'), 'dice');
-
-  // 1. insertLast 사용
-  // 2. template 전달
-  // 3. diceValue interpolation(보간법) 하기 ex)<td>${diceValue}</td>
-
-  insertLast('.recordList tbody', createItem(diceValue));
-  endScroll(recordListWrapper);
-}
-
 function handleRecord() {
   recordListWrapper.hidden = false;
 
-  rederRecordItem();
+  renderRecordItem();
 }
+
 function handleReset() {
   recordListWrapper.hidden = true;
-  clearContents('.tbody');
+  clearContents('tbody');
   count = 0;
   total = 0;
 }
 
-rollingDiceBtn.addEventListener('click', handleRollingDice);
-recordBtn.addEventListener('click', handleRecord);
-resetBtn.addEventListener('click', handleReset);
+rollingButton.addEventListener('click', handleRollingDice);
+recordButton.addEventListener('click', handleRecord);
+resetButton.addEventListener('click', handleReset);
