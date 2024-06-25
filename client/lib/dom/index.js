aa3889031dc070ea9e86cc4e1abe89ef6232b7c1
YSP97 <pus9717@gmail.com>
2024-06-25 11:43:23 +0900

    lib 파일 삭제됨

--- client/lib/dom/index.js
+++
@@ -1,9 +0,0 @@
-// 다시 내보내기 re-export
-
-export * from './css.js';
-export * from './attr.js';
-export * from './insert.js';
-export * from './getNode.js';
-export * from './bindEvent.js';
-export * from './clearContents.js';
-export * from './showAlert.js';
--- client/main.js
+++ client/main.js
@@ -1,40 +1,78 @@
-/* global gsap */
+// import {  } from './lib/index.js'; // 실행 안해도 일반 애니메이션으로 건 gsap그거는 이렇게 연결만 해도 실행이 된다고 함...
+import clearContents from './lib/dom/clearContents.js';
+import { endScroll } from './lib/dom/endScroll.js';
+import {
+  diceAnimation,
+  getNodes,
+  getNode,
+  attr,
+  insertLast,
+} from './lib/index.js';
 
-import { attr } from '../dom/attr.js';
-import { getNode } from '../dom/getNode.js';
+// 1. 주사위 애니메이션
+// 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation 실행될 수 있도록
 
-const cube = getNode('#cube');
+const btnGroupt = getNodes('.buttonGroup > button');
 
-let random;
-gsap.to(cube, {
-  duration: 1,
-  rotationX: 100,
-  rotationY: -100,
-  ease: 'back(10)',
-});
+const [rollingDiceBtn, recordBtn, resetBtn] = btnGroupt;
 
-export function diceAnimation() {
-  random = gsap.utils.random([0, 1, 2, 3, 4, 5]);
+const handleRollingDice = (() => {
+  let isClicked = false;
+  let stopAnimation;
 
-  function complete() {
-    attr(cube, 'dice', random + 1);
-  }
+  return () => {
+    if (!isClicked) {
+      stopAnimation = setInterval(diceAnimation, 100);
+      recordBtn.disabled = true;
+      resetBtn.disabled = true;
+    } else {
+      clearInterval(stopAnimation);
+      recordBtn.disabled = false;
+      resetBtn.disabled = false;
+    }
 
-  const rotationValue = [
-    [0, 0], // 1
-    [0, -90], // 2
-    [-90, 0], // 3
-    [90, 0], // 4
-    [0, 90], // 5
-    [-180, 0], // 6
-  ];
+    isClicked = !isClicked;
+  };
+})();
 
-  gsap.to(cube, {
-    ease: 'linear',
-    duration: 0.2,
-    z: -100,
-    rotationX: rotationValue[random][0],
-    rotationY: rotationValue[random][1],
-    onComplete: complete,
-  });
+const recordListWrapper = getNode('.recordListWrapper');
+let count = 0;
+let total = 0;
+
+function createItem(value) {
+  const template = /* html */ `
+  <tr>
+    <td>${++count}</td>
+    <td>${value}</td>
+    <td>${(total += value)}</td>
+  </tr>`;
+
+  return template;
 }
+
+function rederRecordItem() {
+  const diceValue = +attr(getNode('#cube'), 'dice');
+
+  // 1. insertLast 사용
+  // 2. template 전달
+  // 3. diceValue interpolation(보간법) 하기 ex)<td>${diceValue}</td>
+
+  insertLast('.recordList tbody', createItem(diceValue));
+  endScroll(recordListWrapper);
+}
+
+function handleRecord() {
+  recordListWrapper.hidden = false;
+
+  rederRecordItem();
+}
+function handleReset() {
+  recordListWrapper.hidden = true;
+  clearContents('.tbody');
+  count = 0;
+  total = 0;
+}
+
+rollingDiceBtn.addEventListener('click', handleRollingDice);
+recordBtn.addEventListener('click', handleRecord);
+resetBtn.addEventListener('click', handleReset);
